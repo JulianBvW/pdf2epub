70d128d94c64b65025bcaa928a0fcaa386a580cc
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2023-01-06 21:30:43 +0100

    Integrated new code; Remove Page Num doesn't work :/

--- pdf2epub.py
+++ pdf2epub.py
@@ -16,11 +16,11 @@ parser.add_argument('file', type=str, help='the PDF file you want to convert')
 parser.add_argument('title', type=str, help='the title of the book')
 parser.add_argument('author', type=str, help='the name of the author')
 
-parser.add_argument('-c', '--cover', type=int, default=1, metavar='N', help='the page of the cover image (-1 if none), default=1')
+parser.add_argument('-c', '--cover', type=int, default=1, metavar='N', help='the page of the cover image (0 if none), default=1')
 parser.add_argument('-f', '--first', type=int, default=2, metavar='N', help='the first page containing actual text to convert, default=2')
 parser.add_argument('-s', '--image_scale', type=int, default=4, metavar='N', help='the multiplier for image resolution, default=4')
 parser.add_argument('-l', '--language', type=str, default='de', metavar='', help='the specified language, default=de')
-parser.add_argument('-p', '--pagenumbers', type=int, default=-1, metavar='', help='the page where page numbers start (-1 if none), default=-1')
+parser.add_argument('-p', '--pagenumbers', type=int, default=0, metavar='', help='the page where page numbers start (0 if none), default=-1')
 
 args = parser.parse_args()
 
@@ -32,6 +32,13 @@ print(args.first)
 print(args.image_scale)
 print(args.language)
 print(args.pagenumbers)
+
+UNCERTAINTY_END = 10
+UNCERTAINTY_SIZE = 2
+UNCERTAINTY_LINE_HEIGTH = 2
+
+PAGE_NUM_PATTERN = r'[0-9]+'
+#PAGE_NUM_PATTERN = r'\- [0-9]+ \-'
 
 HTML_CHARS = [('&', '&amp;'), ('<', '&lt;'), ('>', '&gt;')]
 
@@ -59,166 +66,205 @@ def get_flags(flags):
         l.append('bold')
     return l
 
-def remove_pagenum(blocks):
-    filtered_blocks = []
-    for block in blocks:
-        if not (len(block) == 1 and block[0]['text'].lstrip().isdecimal()):
-            filtered_blocks.append(block)
-    if len(blocks) > len(filtered_blocks)+1:
-        print('WARNING: TOO MANY LINES REMOVED')
-        print(blocks)
-    return filtered_blocks
-
 def htmlify(text):
     for (pat, sub) in HTML_CHARS:
         text = re.sub(pat, sub, text)
     return text
+
+def same(a, b, uncertainty):
+    return a <= b + uncertainty and a >= b - uncertainty
 
 ### Save Image
 
 def save_image(page, bbox, img_nr):
     page.get_pixmap(matrix=fitz.Matrix(args.image_scale, args.image_scale), clip=bbox).pil_save(f'tmp/img-{img_nr}.jpeg')
 
-### Spans & Lines
+### Extract Content
 
-def extract_span(span):
-    flags = get_flags(span['flags'])
-    size = span['size']
-    text = span['text']
-    text = htmlify(text)
-    if 'italic' in flags:
-        text = f'<i>{text}</i>'
-    if 'bold' in flags:
-        text = f'<b>{text}</b>'
-    if 'monospaced' in flags:
-        text = f'<tt>{text}</tt>'
-    if 'superscript' in flags:
-        text = f'<sup>{text}</sup>'
-    return {'text': text, 'size': round(size)}
-
-def extract_line(line):
-    text = ''
-    size = -1
-    for span in line['spans']:
-        span_info = extract_span(span)
-        text += span_info['text']
-        if span_info['text'].lstrip() != '' and size == -1:
-            size = span_info['size']
-    end_pos = round(line['bbox'][2])
-    return {'text': text.rstrip(), 'size': size, 'end_pos': end_pos}
-
-def get_lines_from_block(page, pagenum, blocks, img_nr):
-    page_blocks = []
-    for block in blocks:
-        block_lines = []
-
-        # Handle TEXT Blocks
-        if block['type'] == 0:
-            for line in block['lines']:
-                line_info = extract_line(line)
-                if line_info['text'] != '':
-                    block_lines.append(line_info)
-
-        # Handle IMAGE Blocks
-        if block['type'] == 1:
-            save_image(page, block['bbox'], img_nr)
-            block_lines.append({'text': f'<img width="90%" src="img-{img_nr}.jpeg"/>', 'size': 11, 'end_pos': -1})
-            img_nr += 1
-
-        page_blocks.append(block_lines)
-
-    if args.pagenumbers-1 >= 0 and pagenum >= args.pagenumbers-1:
-        page_blocks = remove_pagenum(page_blocks)
-
-    page_lines = sum(page_blocks, []) # Flatten list of lists
-
-    return page_lines, img_nr
-
-
-def combine_all_lines(pdf):
+def get_spans(book, starting_page=1, pagenumber_start=0):
+    spans = []
     img_nr = 0
+    for page_nr in range(starting_page-1, book.page_count):
+        blocks = book[page_nr].get_text('dict', flags=11)['blocks']
+        for block in blocks:
+
+            # Handle TEXT Blocks
+            if block['type'] == 0:
+
+                # Remove the page number if needed
+                if pagenumber_start > 0 and page_nr >= pagenumber_start-1 and len(block['lines']) == 1 and len(block['lines'][0]['spans']) == 1 and bool(re.match(PAGE_NUM_PATTERN, block['lines'][0]['spans'][0]['text'].strip())):
+                   continue
+
+                # Flatten all Spans in all Lines
+                for line in block['lines']:
+                    for span in line['spans']:
+                        if span['text'].strip() != '':
+                            spans.append({
+                                'text': htmlify(span['text']),
+                                'size': round(span['size']),
+                                'y_org': round(span['origin'][1]),
+                                'x_end': round(span['bbox'][2]),
+                                'flags': span['flags']
+                            })
+
+            # Handle IMAGE Blocks
+            if block['type'] == 1:
+                save_image(book[page_nr], block['bbox'], img_nr)
+                spans.append({
+                    'text': f'<img width="90%" src="img-{img_nr}.jpeg"/>',
+                    'size': -1, 'y_org': -1, 'x_end': -1, 'flags': -1
+                })
+                img_nr += 1
+
+    return spans
+
+def group_spans_in_lines(spans):
     lines = []
-    for pagenum, page in enumerate(pdf.pages(args.first-1)):
-        blocks = page.get_text('dict')['blocks']
-        new_lines, img_nr = get_lines_from_block(page, pagenum+args.first-1, blocks, img_nr)
-        lines += new_lines
+    current_line_y = spans[0]['y_org']
+    current_span_list = []
+    for span in spans:
+        if same(span['y_org'], current_line_y, UNCERTAINTY_LINE_HEIGTH):
+            current_span_list.append(span)
+        else:
+            lines.append(current_span_list)
+            current_line_y = span['y_org']
+            current_span_list = [span]
+    lines.append(current_span_list)
     return lines
 
-### Common Numbers
-
-def get_common_numbers(lines):
-    sizes = []
-    end_poses = []
-    for line in lines:
-        sizes.append(line['size'])
-        end_poses.append(line['end_pos'])
-    s_c = Counter(sizes).most_common()
-    ep_c = Counter(end_poses).most_common()
-    return {'sizes': s_c, 'end_poses': ep_c}
-
-### Paragraphs
-
-def lines_to_paragraphs(lines, final_end_pose, end_pos_uncertainty):
-    paragraphs = []
-
-    # Current paragraph data
-    current_p_text = ''
-    current_p_size = lines[0]['size']
-
-    # Go through every line
+def get_text_constants(lines):
+    text_sizes = {}
+    text_ends = {}
     for line in lines:
 
-        # If a new paragraph starts
-        if line['size'] != current_p_size:
+        span = line[-1]
+        text_len, text_size, text_end = len(span['text']), span['size'], span['x_end']
 
-            # Only save the paragraph if there is something to save
-            if current_p_text:
-                paragraphs.append({'text': current_p_text, 'size': current_p_size})
-            
-            # Reset current paragraph data
-            current_p_text = ''
-            current_p_size = line['size']
+        if not (text_size in text_sizes):
+            text_sizes[text_size] = 0
+        text_sizes[text_size] += text_len
+
+        if not (text_end in text_ends):
+            text_ends[text_end] = 0
+        text_ends[text_end] += 1
         
-        # Add the current data
-        current_p_text += line['text'] + ' '
+    return max(text_sizes, key=text_sizes.get), max(text_ends, key=text_ends.get)
 
-        # If a word is split to the next line, remove the seperation
-        if line['end_pos'] >= final_end_pose - end_pos_uncertainty and current_p_text[-2:] == '- ' and current_p_text[-3] != ' ':
-            current_p_text = current_p_text[:-2]
+def lines_to_paragraphs(lines, text_size, end_pos):
+    paragraphs = []
+    current_paragraph_spans = []
+    current_paragraph_size = lines[0][0]['size']
+    for line in lines:
 
-        # If a line ends before it reaches the right side, a new paragraph starts
-        if line['end_pos'] < final_end_pose - end_pos_uncertainty:
-            paragraphs.append({'text': current_p_text, 'size': current_p_size})
-            current_p_text = ''
-            current_p_size = line['size']
-            
-    # Add the final paragraph
-    paragraphs.append({'text': current_p_text, 'size': current_p_size})
+        # If a new size comes, create a new paragraph
+        if not same(line[0]['size'], current_paragraph_size, UNCERTAINTY_SIZE):
+            paragraphs.append({
+                'size': current_paragraph_size,
+                'spans': current_paragraph_spans
+            })
+            current_paragraph_spans = []
+            current_paragraph_size = line[0]['size']
+        
+        # Add the spans to the current paragraph
+        for span in line:
+            current_paragraph_spans.append({
+                'text': span['text'],
+                'size': span['size'],
+                'flags': span['flags']
+            })
+
+        # Remove '-' from line break if a word got cut
+        if current_paragraph_spans[-1]['text'][-1] == '-':
+            current_paragraph_spans[-1]['text'] = current_paragraph_spans[-1]['text'][:-1]
+        elif current_paragraph_spans[-1]['text'][-1] != ' ':
+            current_paragraph_spans[-1]['text'] = current_paragraph_spans[-1]['text'] + ' '
+
+        # End the paragraph if the lines doesn't go until the end
+        if line[-1]['x_end'] <= end_pos - UNCERTAINTY_END and line[0]['size'] <= text_size + UNCERTAINTY_SIZE:
+            paragraphs.append({
+                'size': current_paragraph_size,
+                'spans': current_paragraph_spans
+            })
+            current_paragraph_spans = []
+            current_paragraph_size = line[0]['size']
+    
+    # Add the last paragraph
+    paragraphs.append({
+        'size': current_paragraph_size,
+        'spans': current_paragraph_spans
+    })
+
     return paragraphs
 
-def paragraphs_to_chapters(paragraphs, final_text_size, book_title, text_size_uncertainty):
-    chapters = []
-    title = book_title
-    texts = []
-    for para in paragraphs:
-        if para['size'] > final_text_size + text_size_uncertainty:
-            chapters.append({'title': title, 'texts': texts})
-            title = para['text']
-            texts = []
-        elif para['size'] < final_text_size - text_size_uncertainty:
-            texts.append('<small>' + para['text'] + '</small>')
-        else:
-            texts.append(para['text'])
-    chapters.append({'title': title, 'texts': texts})
-    return chapters
+def combine_spans(paragraphs, text_size):
 
-def get_chapters(pdf, book_title, end_pos_uncertainty=5, text_size_uncertainty=1):
-    lines = combine_all_lines(pdf)
-    commons = get_common_numbers(lines)
-    final_text_size = commons['sizes'][0][0]
-    final_end_pose  = commons['end_poses'][0][0]
-    paragraphs = lines_to_paragraphs(lines, final_end_pose, end_pos_uncertainty)
-    chapters = paragraphs_to_chapters(paragraphs, final_text_size, book_title, text_size_uncertainty)
+    def inner_combine(paragraph):
+        paragraph_text = ''
+        for span in paragraph['spans']:
+            text = span['text']
+            flags = get_flags(span['flags'])
+            if 'italic' in flags:
+                text = '<i>' + text + '</i>'
+            if 'bold' in flags:
+                text = '<b>' + text + '</b>'
+            if 'monospaced' in flags:
+                text = '<tt>' + text + '</tt>'
+            if 'superscript' in flags:
+                text = '<sup>' + text + '</sup>'
+            if span['size'] < text_size:
+                text = '<small>' + text + '</small>'
+            paragraph_text += text
+        return {
+            'size': paragraph['size'],
+            'text': paragraph_text
+        }
+    
+    return list(map(inner_combine, paragraphs))
+
+def paragraphs_to_chapters(paragraphs, text_size, book_title):
+    chapters = []
+    current_chapter_title = book_title
+    current_chapter_content = ''
+
+    for paragraph in paragraphs:
+        
+        # If the text size is great, create a new chapter
+        if paragraph['size'] >= text_size + UNCERTAINTY_SIZE:
+            chapters.append({
+                'title': current_chapter_title,
+                'content': current_chapter_content
+            })
+            current_chapter_title = paragraph['text']
+            current_chapter_content = ''
+
+        # Else add the paragraph
+        else:
+            current_chapter_content += '<p>' + paragraph['text'] + '</p>'
+        
+    # Add final chapter
+    chapters.append({
+        'title': current_chapter_title,
+        'content': current_chapter_content
+    })
+    
+    return chapters[1:] # TODO where to start if first p is empty
+
+def get_chapters(pdf, book_title, starting_page):
+    
+    # Get PDF Content
+    spans = get_spans(pdf, starting_page=starting_page)
+    lines = group_spans_in_lines(spans)
+
+    # Find Constants for text size and line width
+    text_size, end_pos = get_text_constants(lines)
+    print(f'Found ({text_size=}, {end_pos=})')
+    # TODO: reset all '-1' sizes to {text_size}
+
+    # Create Chapters
+    paragraphs = lines_to_paragraphs(lines, text_size, end_pos)
+    paragraphs = combine_spans(paragraphs, text_size)
+    chapters = paragraphs_to_chapters(paragraphs, text_size, book_title)
+
     return chapters
 
 #######################
@@ -235,23 +281,21 @@ def main():
     book.set_identifier(''.join(random.choice(['0','1','2','3','4','5','6','7','8','9']) for i in range(30)))
 
     # Cover
-    if args.cover != -1:
+    if args.cover != 0:
         cover_page = pdf[args.cover-1]
         for block in cover_page.get_text('dict')['blocks']:
             if block['type'] == 1: # Image
-                save_image(cover_page, block['bbox'], 'cover')
+                save_image(cover_page, block['bbox'], 'cover') # TODO nicht immer ganzes bild im block?? example: hp6.pdf
         with open('tmp/img-cover.jpeg', 'rb') as c:
             book.set_cover('cover.jpeg', c.read())
 
     # Content
-    chapters = get_chapters(pdf, args.title)
+    chapters = get_chapters(pdf, args.title, args.first)
 
     contents = []
     for nr, chapter in enumerate(chapters):
         title = chapter['title']
-        html = f'<h1>{title}</h1>'
-        for para_text in chapter['texts']:
-            html += f'<p>{para_text}</p>'
+        html = '<h1>' + title + '</h1>' + chapter['content']
         ch = epub.EpubHtml(title=re.sub('<..?.?>', '', title), file_name=f'ch-{nr}.xhtml', lang='de')
         ch.set_content(html)
         book.add_item(ch)
